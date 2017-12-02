e4ddb4a92732584138fc67e50a11c763757c4fc8
Zach Miller <zmmille2@gmail.com>
2017-12-01 16:50:19 -0800

    day 1

--- 2017/day1.hs
+++ 2017/day1.hs
@@ -1,2 +1,13 @@
-foo :: Int -> Int
-foo n = n
+import Data.Char
+
+repeatSum :: String -> Int
+repeatSum [] = 0
+repeatSum xs = repeatSumHelper ys 0
+    where ys = (last xs):xs
+
+repeatSumHelper :: String -> Int -> Int
+repeatSumHelper [] runningTotal = runningTotal
+repeatSumHelper (x:(y:xs)) runningTotal
+    | x == y    = repeatSumHelper (y:xs) ((digitToInt y) + runningTotal)
+    | otherwise = repeatSumHelper (y:xs) runningTotal
+repeatSumHelper _ runningTotal = runningTotal
