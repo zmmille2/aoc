bd77f57c30f5efa064bb28833a7fdbe50d461f27
Zach Miller <zmmille2@gmail.com>
2018-03-23 10:19:26 -0700

    added day5, debugging

--- 2017/day5.hs
+++ 2017/day5.hs
@@ -1,6 +1,7 @@
 import Data.Char
 import Data.List
 import Data.List.Split
+import Control.Lens
 
 runProgram = do
     all <- readFile "inputs/day5.txt"
@@ -8,9 +9,13 @@ runProgram = do
     let nums = map (\x -> read x :: Int) rows
     return (run 0 nums 0)
 
+run :: Int -> [Int] -> Int -> Int
 run index program steps
-    | index > length program = steps
-    | otherwise              = run index' program' (steps + 1)
+    | index >= (length program) = steps
+    | otherwise                 = run (index') program' (steps + 1)
     where
-        index' = index + program[index]
-        program' = program
+        value = program!!index
+        index' = index + value
+        program' = program & element index .~ (value + 1)
+
+-- 1075 too low: forgot to replace index + 1 with index' lol
